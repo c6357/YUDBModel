df9d065405f3e047a5cf92278889c65ef89ddfde
yuzhx <c6357@outlook.com>
2015-08-25 17:10:06 +0800

    error

--- YUDBObjectSample/build/YUDBObjectSample.build/Debug-iphonesimulator/YUDBObjectSample.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/yzx/Desktop/github/YUDBObject/YUDBFramework/YUDBObjectSample/YUDBObjectSample/AppDelegate.m \
-  /Users/yzx/Desktop/github/YUDBObject/YUDBFramework/YUDBObjectSample/YUDBObjectSample/AppDelegate.h
--- YUDBObjectSample/build/YUDBObjectSample.build/Debug-iphonesimulator/YUDBObjectSample.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/yzx/Desktop/github/YUDBObject/YUDBFramework/YUDBObjectSample/YUDBObjectSample/main.m \
-  /Users/yzx/Desktop/github/YUDBObject/YUDBFramework/YUDBObjectSample/YUDBObjectSample/AppDelegate.h
