9ad9e963920266344e58ada498baa869240998f6
6357 <c6357@outlook.com>
2015-08-29 00:27:17 +0800

    dev：提交

---
+++ YUDBObjectSample/DBObj.h
@@ -0,0 +1,24 @@
+//
+//  DBObj.h
+//  YUDBObject
+//
+//  Created by BruceYu on 15/8/18.
+//  Copyright (c) 2015年 BruceYu. All rights reserved.
+//
+
+
+#import <YUDBFramework/DBOBject.h>
+#import "UserInfo.h"
+
+
+@interface DBObj : DBObject
+
+@property (strong,nonatomic)NSString *name;
+
+@property (strong,nonatomic)NSString *phone;
+
+@property (strong,nonatomic)UserInfo *info;
+
+@property (strong,nonatomic)NSArray *infoArry;
+
+@end
